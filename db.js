c778ee3da83af742c8cbe76e19abf3ec8aa14b47
Sid9879 <singhsiddharth1438@gmail.com>
2025-06-27 16:16:29 +0530

    nwedb

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const connectToDb = ()=>{
-    mongoose.connect(`mongodb+srv://${process.env.API_KEY}:${process.env.API_SECRET}@expense.b3w5z.mongodb.net/`)
+    mongoose.connect(process.env.API_KEY)
     .then(()=>console.log('Connected to MongoDB'))
     .catch(()=>console.log('error in connecting mongoosedb'))
 }
